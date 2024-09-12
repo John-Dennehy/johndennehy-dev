be98ddd56491a00336a14dfdcfc9628bb5c65569
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-09-12 18:48:14 +0100

    chore: Update Media configuration for image sizes and disable local storage

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -39,7 +39,33 @@ export const Media: CollectionConfig = {
     },
   ],
   upload: {
-    // Upload to the public/media directory in Next.js making them publicly accessible even outside of Payload
-    staticDir: path.resolve(dirname, '../../public/media'),
+    disableLocalStorage: true,
+    
+    imageSizes: [
+      {
+        name: 'thumbnail',
+        width: 400,
+        height: 300,
+        position: 'centre',
+      },
+      {
+        name: 'card',
+        width: 768,
+        height: 1024,
+        position: 'centre',
+      },
+      {
+        name: 'tablet',
+        width: 1024,
+        // By specifying `undefined` or leaving a height undefined,
+        // the image will be sized to a certain width,
+        // but it will retain its original aspect ratio
+        // and calculate a height automatically.
+        height: undefined,
+        position: 'centre',
+      },
+    ],
+    adminThumbnail: 'thumbnail',
+    mimeTypes: ['image/*'],
   },
 }
